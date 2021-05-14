47b10bc693e9b2018fafe78fb071c0e31e2978fb
Yasin Arafat <yasinarafat1194@gmail.com>
2021-05-15 00:47:05 +0600

    passwordencoder

--- src/main/java/com/example/demo/entity/user/RoleType.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity.user;
-
-
-public enum RoleType {
-    ROLE_SUPPER_ADMIN,
-    ROLE_ADMIN,
-    ROLE_CUSTOMER
-
-}
--- src/main/java/com/example/demo/security/ApplicationSecurity.java
+++ src/main/java/com/example/demo/security/ApplicationSecurity.java
@@ -1,14 +1,27 @@
 package com.example.demo.security;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurity(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -20,4 +33,18 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails rofiqUser=User.builder()
+                .username("rofiq")
+                .password(passwordEncoder.encode("password"))
+                .roles("")
+                .build();
+        return new InMemoryUserDetailsManager(
+                rofiqUser
+        );
+    }
 }
+
