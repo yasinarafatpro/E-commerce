b9bf20dd784644a8ab901b2df5eae0e191ec6876
Yasin Arafat <yasinarafat1194@gmail.com>
2021-05-23 23:58:11 +0600

    jwt usernamepassword and authenticatee and token create

--- src/main/java/com/example/demo/jwt/jwtUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/jwtUsernamePasswordAuthenticationFilter.java
@@ -1,15 +1,23 @@
 package com.example.demo.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.javafx.css.StyleCacheEntry;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class jwtUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -35,5 +43,23 @@ public class jwtUsernamePasswordAuthenticationFilter extends UsernamePasswordAut
             throw new RuntimeException(e);
         }
 
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+
+        String key = "securesecuresecuresecuresecuresecuresecuresecuresecuresecure";
+
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(SignatureAlgorithm.HS256,"secretsecretsecretsecretsecretsecret=".getBytes())
+                .compact();
+        response.addHeader("Authorization","Bearer" +token);
     }
 }
