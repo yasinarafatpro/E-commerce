8b0a174906264e1103b8cdbac4a17aa11865b014
Yasin Arafat <yasinarafat1194@gmail.com>
2021-06-11 22:56:28 +0600

    orderdetailservice

--- src/main/java/com/example/demo/service/OrderDetailService.java
+++ src/main/java/com/example/demo/service/OrderDetailService.java
@@ -1,7 +1,13 @@
 package com.example.demo.service;
 
-/**
- * Created by user on 6/11/2021.
- */
+import com.example.demo.entity.orderdetails.OrderDetail;
+
+import java.util.List;
+
 public interface OrderDetailService {
+    OrderDetail create(OrderDetail orderDetail);
+    OrderDetail update(OrderDetail orderDetail);
+    OrderDetail findById(String id);
+    List<OrderDetail> findAll();
+    void deleteById(String id);
 }
---
+++ src/main/java/com/example/demo/service/orderdetailserviceimpl/OrderDetailServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.orderdetailserviceimpl;
+
+import com.example.demo.entity.orderdetails.OrderDetail;
+import com.example.demo.service.OrderDetailService;
+
+import java.util.List;
+
+public class OrderDetailServiceImpl implements OrderDetailService{
+    @Override
+    public OrderDetail create(OrderDetail orderDetail) {
+        return null;
+    }
+
+    @Override
+    public OrderDetail update(OrderDetail orderDetail) {
+        return null;
+    }
+
+    @Override
+    public OrderDetail findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<OrderDetail> findAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(String id) {
+
+    }
+}
