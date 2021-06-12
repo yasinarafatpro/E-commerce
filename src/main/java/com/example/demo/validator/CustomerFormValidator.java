74f7985efdd1da2c97b08de5b47b0cc4e5278a79
Yasin Arafat <yasinarafat1194@gmail.com>
2021-06-12 19:40:46 +0600

    model cartinfo cartline productinfo..validator...edit

--- src/main/java/com/example/demo/validator/CustomerFormValidator.java
+++ src/main/java/com/example/demo/validator/CustomerFormValidator.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
@@ -14,6 +15,7 @@ import org.springframework.validation.Validator;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Component
 public class CustomerFormValidator implements Validator{
     private EmailValidator emailValidator;
 
@@ -24,8 +26,14 @@ public class CustomerFormValidator implements Validator{
 
     @Override
     public void validate(Object target, Errors errors) {
-        CustomerForm customerForm=(CustomerForm)target;
-    }
+        CustomerForm custInfo=(CustomerForm)target;
+        //check the fields of productForm
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"code","NotEmpty.productForm.code");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"name","NotEmpty.productForm.name");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"price","NotEmpty.productForm.price");
 
-    
+        if (emailValidator.isValid(custInfo.getEmail())){
+            errors.rejectValue("email","Pattern.customerForm.email");
+        }
+    }
 }
